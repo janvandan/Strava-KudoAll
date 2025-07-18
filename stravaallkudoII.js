324e44853484a5f1080f1da455274d8129562184
Van Dravik <jan@vandravik.com>
2025-07-18 16:36:11 +0200

    Update stravaallkudoII.js

--- stravaallkudoII.js
+++ stravaallkudoII.js
@@ -117,12 +117,21 @@
 			inputButton.value="Give kudo to ALL n" + pageActivite;
 			
 			if ( idActiviteActive === idActiviteNext ) {
-					
+				
 				lastDiv = document.getElementsByClassName("MIt1i")[0]; // class "MIt1i", dernier div de bas de page
 				lastDivPrevSibling = lastDiv.previousElementSibling; // recup de la derniere activite avant "MIt1i"
 				idActiviteNext = lastDivPrevSibling.getElementsByClassName("CQdSY")[0].id; // recup de l'id de la derniere activite
+					
+				setTimeout(() => if ( debug > 1 ) {
+					console.log(nameJavaScript + ".chargeMaxActivite : (idActiviteActive, idActiviteNext) = (1) (" + idActiviteActive + ", " + idActiviteNext + ")" );
+				}, 1000);
+				setTimeout(() => if ( debug > 1 ) {
+					console.log(nameJavaScript + ".chargeMaxActivite : (idActiviteActive, idActiviteNext) = (2) (" + idActiviteActive + ", " + idActiviteNext + ")" );
+				}, 2000);
+				setTimeout(() => if ( debug > 1 ) {
+					console.log(nameJavaScript + ".chargeMaxActivite : (idActiviteActive, idActiviteNext) = (3) (" + idActiviteActive + ", " + idActiviteNext + ")" );
+				}, 3000);
 
-				if ( debug > 1 ) { console.log(nameJavaScript + ".chargeMaxActivite : (idActiviteActive, idActiviteNext) = (" + idActiviteActive + ", " + idActiviteNext + ")" ); }
 			}
 
 			tagTestFinAffichageActivites = document.getElementsByClassName("f5jBr JlaW0");
